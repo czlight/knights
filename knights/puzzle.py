d13fa64077f932777b3fc30ee1399ca34a062020
czlight <alejandro.r1212@gmail.com>
2024-08-09 18:03:01 +0000

    Fri, Aug 9, 2024, 1:03 PM -05:00

--- knights/puzzle.py
+++ knights/puzzle.py
@@ -11,11 +11,11 @@ CKnave = Symbol("C is a Knave")
 
 
 
-# Puzzle 0
+# Puzzle 0 ∧ ¬
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    AKnight => Symbol,
-    AKnave
+    AKnight => Sentence(Symbol),
+    AKnave => ¬Sentence(Symbol)
 
 )
 
